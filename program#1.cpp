cd4e0dfdd49a7eb84b538e12378b7138f2337b5a
SABUYA, Marvic N. <165488772+dexluna@users.noreply.github.com>
2025-02-26 15:29:12 +0800

    program#1.cpp

--- program#1.cpp
+++ program#1.cpp
@@ -3,31 +3,42 @@
 using namespace std;
 
 int main() {
+
+    // dclare variables to store user's name and birth details
     string name; 
     int birthYear, birthMonth, birthDay; 
+    
+    // define current date constants
     const int currentYear = 2025;
     const int currentMonth = 2; 
-    const int currentDay = 21; 
+    const int currentDay = 26; 
 
-    cout << "Enter your name: ";
-    cin >> name; 
-
-    cout << "Enter your birth year: ";
-    cin >> birthYear; 
-
-    cout << "Enter your birth month: ";
-    cin >> birthMonth; 
-
-    cout << "Enter your birth day: ";
-    cin >> birthDay; 
-
+    // prompt user to input their name
+    cout << "Enter your name: "; 
+    cin >> name;  // read user's name from input
     
-    int age = currentYear - birthYear - ((currentMonth <  birthMonth) || 
+    
+    // prompt user to input their birth year, month, and day
+    cout << "Enter your birth year: "; 
+    cin >> birthYear; // read birth year from input
+    
+    
+    cout << "Enter your birth month: "; 
+    cin >> birthMonth; // read birth month from input
+    
+    cout << "Enter your birth day: ";   
+    cin >> birthDay;  // read birth day from input
+    
+
+    // calculate age using a boolean expression 
+    int age = currentYear - birthYear - ((currentMonth < birthMonth) || 
      (currentMonth == birthMonth && currentDay < birthDay));  
      
-    cout<< endl;
-    cout << "Your name is  " << name << " and you are " <<    age << endl;
+     
+     
+    // display the calculated age
+    cout << endl;
+    cout << "Your name is " << name << " and you are " << age << endl;
 
     return 0;
 }
-    
